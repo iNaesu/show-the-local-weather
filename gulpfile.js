5cbf26c63071af3ea4148e0a720137ac7a4877eb
Sean Law <sean.law.321@gmail.com>
2017-08-01 22:39:08 +1000

    Add gulp to automate browser reload after compiling css

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+
+  gulp.watch("./scss/*.scss", ['sass','sass-watch']);
+  gulp.watch("./*.html").on('change', browserSync.reload);
+  gulp.watch("./js/*.js").on('change', browserSync.reload);
+  gulp.watch("./scss/style.css").on('change', browserSync.reload);
+
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("scss/style.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./scss"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('sass-watch', ['sass'], browserSync.reload);
+gulp.task('default', ['serve']);
